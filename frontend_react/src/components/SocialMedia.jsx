02e4bb7bc23f536700b571e73a5a984b819ba82e
rabby9898 <mdfajlerabby00@gmail.com>
2023-02-06 00:04:12 +0600

    first commit

---
+++ frontend_react/src/components/SocialMedia.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {BsLinkedin, BsInstagram} from "react-icons/bs";
+import {FaFacebookF} from "react-icons/fa"
+
+const SocialMedia = () => {
+  return (
+    <div className='app__social'>
+    <div>
+        <BsLinkedin/>
+    </div>
+    <div>
+        <FaFacebookF/>
+    </div>
+    <div>
+        <BsInstagram/>
+    </div>
+
+    </div>
+  )
+}
+
+export default SocialMedia;
