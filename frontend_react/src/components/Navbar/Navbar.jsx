b30a2f9c8e4e15e1f68de9cdfcc27a8e6d3408e5
rabby9898 <mdfajlerabby00@gmail.com>
2023-03-20 14:22:53 +0600

    Update project

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
                 <img src={images.logoDev} alt='logo' />
             </div>
             <ul className="app__navbar-links">
-                {['home', 'about', 'work', 'skills', 'testimonials', 'contact'].map((item) => (
+                {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item) => (
                     <li className="app__flex p-text" key={`link-${item}`}>
                         <div />
                         <a href={`#${item}`}>{item}</a>
@@ -31,7 +31,7 @@ const Navbar = () => {
 
                         <HiX onClick={() => setToggle(false)}/>
                             <ul>
-                                {['home', 'about', 'work', 'skills', 'testimonials','contact'].map((item) => (
+                                {['home', 'about', 'work', 'skills', 'testimonial','contact'].map((item) => (
                                 <li key={item}>
                                     <a href={`#${item}`} onClick={() => setToggle(false)} >{item}</a>
                                 </li>
